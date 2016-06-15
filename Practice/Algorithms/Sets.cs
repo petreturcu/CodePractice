1e1d1fe83f52cc436f5ee0664fbcff073ada79e0
petreturcu <petre.turcu@carfinance247.co.uk>
2016-06-16 00:24:04 +0100

    PermuteSimple

---
+++ Practice/Algorithms/Sets.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file = "Sets.cs" author = "Petre Turcu" company = "LadApps">
+//      Copyright (c) 16/06/2016
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Algorithms
+{
+    using System;
+
+    public class Sets
+    {
+        public static void PermuteSimple(string set, string prefix = "")
+        {
+            if (set.Length == 0) Console.WriteLine(prefix);
+            else
+            {
+                for (int i = 0; i < set.Length; i++)
+                {
+                    prefix = prefix + set[i];
+                    PermuteSimple(set.Remove(i, 1), prefix);
+                    prefix = prefix.Remove(prefix.Length - 1, 1);
+                }
+            }
+        }
+    }
+}
