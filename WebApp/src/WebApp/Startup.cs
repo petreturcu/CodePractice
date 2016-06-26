56ca6a7ad9be178172d3438c9e1cc68a194adf82
petreturcu <petre.turcu@carfinance247.co.uk>
2016-06-26 11:04:06 +0100

    Basic json config

--- WebApp/src/WebApp/Startup.cs
+++ WebApp/src/WebApp/Startup.cs
@@ -1,16 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApp
 {
+    using System.IO;
+
+    using Microsoft.Extensions.Configuration;
+
     public class Startup
     {
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -20,10 +29,12 @@ namespace WebApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.Run(
+                async (context) =>
+                    {
+                        var greeting = Configuration["greeting"];
+                        await context.Response.WriteAsync(greeting);
+                    });
         }
     }
 }
