b66940643fc0ce7086464f00aa6c0fc9c6f2473b
petreturcu <petre.turcu@carfinance247.co.uk>
2016-06-26 11:49:28 +0100

    Greeting service

--- WebApp/src/WebApp/Startup.cs
+++ WebApp/src/WebApp/Startup.cs
@@ -1,12 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace WebApp
+﻿namespace WebApp
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.DependencyInjection;
     using System.IO;
-
     using Microsoft.Extensions.Configuration;
+    using Services;
 
     public class Startup
     {
@@ -24,15 +23,17 @@ namespace WebApp
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(provider => Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IGreeter greeter)
         {
             app.Run(
                 async (context) =>
                     {
-                        var greeting = Configuration["greeting"];
+                        var greeting = greeter.GetGreeting();
                         await context.Response.WriteAsync(greeting);
                     });
         }
