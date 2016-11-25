86b7183dd5523bdaba00e9174f8cd57bd8b10ce9
petreturcu <turcupeter@yahoo.co.uk>
2016-11-25 23:49:02 +0000

    ProviderTest

---
+++ Practice/ManualProviderTests/ProvidersTest.cs
@@ -0,0 +1,35 @@
+﻿namespace ManualProviderTests
+{
+    using System;
+    using System.Reflection;
+
+    using FluentAssertions;
+
+    using Mono.CompilerServices.SymbolWriter;
+
+    using NUnit.Framework;
+
+    using StaticManualDI;
+    using StaticManualDI.Interfaces;
+
+    [TestFixture]
+    public class ProvidersTest
+    {
+        [Test]
+        public void Providers_WhenInterfaceSpecified_ShouldReturnDefaultConcrete()
+        {
+            // Arrange
+
+
+            // Act
+            var concreteEmail = Providers.GetProvider<IEmailProvider>();
+            var concreteData = Providers.GetProvider<IDataProvider>();
+            var concreteUser = Providers.GetProvider<IUserProvider>();
+
+            // Assert
+            concreteEmail.Should().BeOfType<SmptEmailProvider>();
+            concreteData.Should().BeOfType<SqlServerDataProvider>();
+            concreteUser.Should().BeOfType<SqlServerUserProvider>();
+        }
+    }
+}
