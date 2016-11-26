d7999fc40a1dd6de79732aff3fd1511a2264f1ff
petreturcu <turcupeter@yahoo.co.uk>
2016-11-26 00:05:57 +0000

    String interning

---
+++ Practice/DotNetPractice/Practice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+[TestFixture]
+public class Practice
+{
+    [Test]
+    public void StringInterning_WhenConstantLiterals_ShouldHaveSameReference()
+    {
+        // Arrange
+        var s1 = "This is a string";
+        var s2 = "This is a string";
+
+        // Act
+        var isSameReference = Object.ReferenceEquals(s1, s2);
+
+        // Assert
+        isSameReference.Should().BeTrue("because .NET string interning");
+    }
+
+    [Test]
+    public void StringInterning_WhenRuntimeLiterals_ShouldHaveDifferentReference()
+    {
+        // Arrange
+        var s1 = "This is a";
+        var s2 = " string";
+        var s3 = "This is a string";
+
+        // Act
+        var isSameReference = Object.ReferenceEquals(s1 + s2, s3);
+        var areStringsEqual = s1 + s2 == s3;
+
+        // Assert
+        isSameReference.Should().BeFalse("because too late for .NET string interning");
+        areStringsEqual.Should().BeTrue("because the strings have the same contents");
+    }
+}
